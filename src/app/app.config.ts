ebd5de98e545cd581d3200c5415f71231dd3c363
Ruben <musayelyan1992@gmail.com>
2026-05-06 01:07:18 +0400

    feat: add SSR

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideTranslateService } from '@ngx-translate/core';
 import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
+    provideHttpClient(withFetch()),
     provideTranslateService({ defaultLanguage: 'ru' }),
     provideTranslateHttpLoader({ prefix: '/i18n/', suffix: '.json' }),
+    provideClientHydration(withEventReplay()),
   ],
 };
